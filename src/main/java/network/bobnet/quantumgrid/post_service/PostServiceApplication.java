c9118770a8c9c9179acba88219842898d4c8ffbe
Bobesz <bob@bobnet.network>
2024-08-30 19:33:15 +0200

    QUAN-32 Set up project structure using Spring Boot

---
+++ src/main/java/network/bobnet/quantumgrid/post_service/PostServiceApplication.java
@@ -0,0 +1,16 @@
+package network.bobnet.quantumgrid.post_service;
+
+import lombok.Generated;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Generated
+@SpringBootApplication
+public class PostServiceApplication {
+
+	@Generated
+	public static void main(String[] args) {
+		SpringApplication.run(PostServiceApplication.class, args);
+	}
+
+}
